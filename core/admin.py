3809571168dcc075355c37d3b0883d5e664c05fa
hadeares <111923702+hadeares@users.noreply.github.com>
2022-08-30 19:42:37 -0300

    Primeira Versão

---
+++ core/admin.py
@@ -0,0 +1,18 @@
+from msilib.schema import Class
+from django.contrib import admin
+
+from core.models import Autor, Categoria, Editora, Livros, Compra, ItensCompra
+
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Livros)
+
+
+
+class ItensInLine(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdimin(admin.ModelAdmin):
+    inlines = (ItensInLine,)
