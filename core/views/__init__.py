216ed2b54d5e64528c0669c5a4672bd2e8125835
hadeares <111923702+hadeares@users.noreply.github.com>
2022-09-01 19:48:22 -0300

    dia 3

---
+++ core/views/__init__.py
@@ -0,0 +1,4 @@
+#from .categoriaClass import CategoriaView
+#from .CategoriaApiView import CategoriaDetail, CategoriasList
+#from .CategoriaGeneric import CategoriasDetailGeneric, CategoriasListGeneric
+from .Categoria import CategoriasViewSet
