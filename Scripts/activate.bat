96627d3452f83be643a19d0e1339638dd2322933
hadeares <111923702+hadeares@users.noreply.github.com>
2022-09-12 18:52:43 -0300

    a

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hadea\Documents\GitHub\Livraria-django
+set VIRTUAL_ENV=C:\Users\vinom\OneDrive\Documentos\GitHub\Livraria-django
 
 if not defined PROMPT set PROMPT=$P$G
 
